9b031cf003787688dfe216a988c69bb9b836bb00
devingdaniels <devingdaniels@gmail.com>
2022-10-26 09:38:32 -0700

    Added test files

---
+++ shipFleet.js
@@ -0,0 +1,9 @@
+const Shipyard = [
+  { name: "carrier", length: 5, id: "car5"  , placed: false },
+  { name: "battleship", length: 4, id: "bat4"  , placed: false},
+  { name: "cruiser", length: 3, id: "cru3" , placed: false },
+  { name: "submarine", length: 3, id: "sub3" , placed: false },
+  { name: "destroyer", length: 2, id: "des2" , placed: false }
+];
+
+module.exports = Shipyard;
