42d43c4f3e7307ae3b6d93ac78c3a205507dc8da
devingdaniels <devingdaniels@gmail.com>
2022-10-30 14:11:38 -0700

    Updated webpack config.js

---
+++ src/game.js
@@ -0,0 +1,9 @@
+import createGrid from './placeShips';
+
+import Player from '../test/Player';
+
+const player = Player();
+
+console.log(player);
+
+createGrid();
--- src/placeShips.js
+++ src/placeShips.js
@@ -1,36 +1,42 @@
 /* eslint-disable no-plusplus */
 import './placeShips.css';
 
-function pressed(e) {
-  console.log(e.target.getAttribute('index'));
+function setShip(e) {
+  const y = e.target.getAttribute('yCoord');
+  const x = e.target.getAttribute('xCoord');
+  console.log(`y:${y}, x:${x}`);
 }
 function changeColor(e) {
   e.target.style.backgroundColor = 'blue';
 }
 
 function createGrid() {
+  // Get the parent container
   const gridContainer = document.getElementById('gridContainer');
-
+  // Set the grid size
   const gridSize = 10;
-
+  // Update parent grid rules
   gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
   gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-
-  for (let i = 0; i < gridSize * gridSize; i++) {
-    const newDiv = document.createElement('div');
-    // newDiv.style.userSelect = "none";
-    newDiv.style.margin = '0';
-    newDiv.style.border = '1px solid red';
-    newDiv.style.cursor = 'pointer';
-    newDiv.setAttribute('index', i);
-    newDiv.addEventListener('click', (e) => {
-      pressed(e);
-    });
-    newDiv.addEventListener('mouseover', (e) => {
-      changeColor(e);
-    });
-    gridContainer.appendChild(newDiv);
+  // Add children and children attributes
+  for (let y = 0; y < gridSize; y++) {
+    for (let x = 0; x < gridSize; x++) {
+      const gridSquare = document.createElement('div');
+      // gridSquare.style.userSelect = 'none';
+      gridSquare.style.margin = '0';
+      gridSquare.style.border = '1px solid black';
+      gridSquare.style.cursor = 'pointer';
+      gridSquare.setAttribute('yCoord', y);
+      gridSquare.setAttribute('xCoord', x);
+      gridSquare.addEventListener('click', (e) => {
+        setShip(e);
+      });
+      gridSquare.addEventListener('mouseover', (e) => {
+        changeColor(e);
+      });
+      gridContainer.appendChild(gridSquare);
+    }
   }
 }
 
-createGrid();
+export default createGrid;
